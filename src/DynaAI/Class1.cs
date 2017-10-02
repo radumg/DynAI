8de4c2dc71c2907099fe1bf4b2603efda6c316c6
Radu Gidei <radugidei@gmail.com>
2017-10-02 03:19:21 +0100

    set up solution

---
+++ src/DynaAI/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynaAI
+{
+    public class MachineLearning
+    {
+        #region Linear Regression
+
+
+        #endregion
+
+        #region Classifiers
+
+        #endregion
+
+        #region Helpers
+
+        #endregion
+    }
+}
