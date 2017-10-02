e9ed47471caf35ebb2218b5401471614b6048e7a
Radu Gidei <radugidei@gmail.com>
2017-10-02 03:55:47 +0100

    implemented LinearRegression class & methods
    - constructor
    - Learn()
    - Predict()

--- src/DynAI/MachineLearning.cs
+++ src/DynAI/MachineLearning.cs
@@ -1,24 +1,88 @@
-﻿using System;
+﻿using Accord.Math.Random;
+using Accord.Statistics.Models.Regression.Linear;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DynaAI
+namespace DynAI.MachineLearning
 {
-    public class MachineLearning
+    #region Linear Regression
+
+    /// <summary>
+    /// Class providing support for linear regression ML algorithms
+    /// </summary>
+    public class LinearRegression
     {
-        #region Linear Regression
+        // dataset
+        double[] inputs;
+        double[] outputs;
+        double test;
 
+        // regression
+        SimpleLinearRegression regression;
+        OrdinaryLeastSquares ols;
+        bool learned = false;
+        double result;
 
-        #endregion
+        /// <summary>
+        /// Constructs a new LinearRegression machine.
+        /// </summary>
+        public LinearRegression(List<double> inputList, List<double> outputList)
+        {
+            // validation
+            if (inputList == null || outputList == null) throw new ArgumentNullException("Neither the input list nor the output list can be NULL");
 
-        #region Classifiers
+            // initialise seed value
+            Generator.Seed = new Random().Next();
 
-        #endregion
+            // process input and output lists into arrays
+            inputs = inputList.ToArray();
+            outputs = outputList.ToArray();
 
-        #region Helpers
+            // set up linear regression using OLS
+            regression = new SimpleLinearRegression();
+            ols = new OrdinaryLeastSquares();
+        }
 
-        #endregion
+        /// <summary>
+        /// Use the object's inputs and outputs to learn the model of the linear regression, using OrdinaryLeastSquares
+        /// </summary>
+        public void Learn()
+        {
+            regression = ols.Learn(inputs, outputs);
+            learned = true;
+        }
+
+        /// <summary>
+        /// Using the learned model, predict an output for the specified input
+        /// </summary>
+        /// <param name="value">The value to use as input for the prediction</param>
+        /// <returns>The predicted value</returns>
+        public double Predict(double value)
+        {
+            // don't predict if we haven't learned the model yet
+            if (learned != true) throw new Exception("Cannot predict before the machine has learned.");
+
+            // check we haven't already predicted for this input
+            if (value == test && learned == true) return result;
+
+            // predict
+            test = value;
+            result = regression.Transform(test);
+
+            return result;
+        }
     }
+    #endregion
+
+    #region Classifiers
+
+    #endregion
+
+    #region Helpers
+
+    #endregion
+
 }
--- src/DynAI/Properties/AssemblyInfo.cs
+++ src/DynAI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynaAI")]
+[assembly: AssemblyTitle("DynAI Machine Learning")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DynaAI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("DynAI Machine Learning")]
+[assembly: AssemblyCopyright("Copyright © Radu Gidei 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
