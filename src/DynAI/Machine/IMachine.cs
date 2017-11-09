98d70bcf088db493af2da159684ab1852ac7b76a
Radu Gidei <radugidei@gmail.com>
2017-11-09 21:16:00 +0000

    added IMachine interface and MachineBase class

---
+++ src/DynAI/Machine/IMachine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    /// <summary>
+    /// The generic capabilities of a machine, used for machine learning
+    /// </summary>
+    public interface IMachine
+    {
+        /// <summary>
+        /// Enables a machine to learn from training data.
+        /// </summary>
+        /// <param name="machine">The machine that will learn.</param>
+        /// <param name="trainingData">The data that will be used to train the machine's learning.</param>
+        /// <returns>The input machine, now trained.</returns>
+        IMachine Learn(IMachine machine, ITrainingData trainingData);
+
+        /// <summary>
+        /// Enables a machine to provide a prediction from an input value.
+        /// </summary>
+        /// <param name="machine">The machine that will predict.</param>
+        /// <param name="inputData">Input for the prediction</param>
+        /// <returns>The predicted value</returns>
+        double Predict(IMachine machine, double inputData);
+    }
+
+    /// <summary>
+    /// This provides an abstraction so that different machines can use different formats for training data.
+    /// </summary>
+    public interface ITrainingData
+    {
+        /// <summary>
+        /// Fetches the training data.
+        /// </summary>
+        /// <returns>The training dataset.</returns>
+        double[] GetTrainingData();
+
+        /// <summary>
+        /// Allows us to set the training data.
+        /// </summary>
+        /// <returns>True if the operation succeeded, false otherwise.</returns>
+        bool SetTrainingData();
+    }
+
+    public interface IInputData<T>
+    {
+        /// <summary>
+        /// Retrieves the input data.
+        /// </summary>
+        /// <returns>The data.</returns>
+        T GetInputData();
+
+        /// <summary>
+        /// Sets the input data for the machine to the specified values.
+        /// </summary>
+        /// <returns>True if the operation succeeded, false otherwise.</returns>
+        bool SetInputData();
+    }
+}
---
+++ src/DynAI/Machine/MachineBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Machine
+{
+    public abstract class MachineBase : IMachine
+    {
+        #region public properties
+        // input data
+        public IInputData<double> Inputs { get; set; }
+        public virtual double[] Outputs { get; set; }
+        public virtual double TestValue { get; set; }
+
+        // training data
+        ITrainingData TrainingData { get; set; }
+
+        // state & result
+        public bool Learned { get; set; }
+        public double Result { get; set; }
+        #endregion
+
+        #region IMachine methods
+        public abstract IMachine Learn(IMachine machine, ITrainingData trainingData);
+
+        public abstract double Predict(IMachine machine, double inputData);
+
+        #endregion
+    }
+}
