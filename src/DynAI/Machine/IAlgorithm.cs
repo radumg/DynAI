acfc26da26e6c55f7702801b8cf530cf80635bcb
Radu Gidei <radugidei@gmail.com>
2018-02-25 20:51:26 +0000

    added IAlgorithm
    this is an abstraction layer to enable #7

---
+++ src/DynAI/Machine/IAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.DesignScript.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Machine
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public interface IAlgorithm
+    {
+        // Metadata and State
+        string Name { get; set; }
+        AlgorithmType Type { get; }
+        bool HasTrainingDataLoaded { get; }
+        bool IsTrained { get; set; }
+
+        // dataset
+        object LastResult { get; }
+        object LastTestValue { get; }
+
+        // Type support
+        Type PredictionType { get; }
+        Type ResultType { get; }
+
+        // ML Methods
+        void Learn();
+        object Predict(object inputData);
+    }
+
+    [IsVisibleInDynamoLibrary(false)]
+    public enum AlgorithmType
+    {
+        Supervised,
+        Unsupervised
+    }
+}
