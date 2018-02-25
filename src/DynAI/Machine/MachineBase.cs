6abc37ed3b952474b3a8c4541c99bcd860d13496
Radu Gidei <radugidei@gmail.com>
2018-02-25 20:56:32 +0000

    update MachineBase to interfaces
    - update MachineBase to use IAlgorithm interface & IMachine changes
    - also get rid of RegressionMachine

--- src/DynAI/Machine/MachineBase.cs
+++ src/DynAI/Machine/MachineBase.cs
@@ -1,59 +1,155 @@
 ﻿using System;
 using AI.Utilities;
 using Autodesk.DesignScript.Runtime;
+using AI.Machine;
 
 namespace AI.Machine
 {
-    //[IsVisibleInDynamoLibrary(false)]
+    /// <summary>
+    /// Machines are entities that can leverage different algorithms to learn from training data and then predict new values.
+    /// </summary>
     public class Machine : IMachine
     {
         #region Metadata
 
-        // metadata
+        /// <summary>
+        /// The name of the machine. Useful when storing the results to disk, does not affect results in any way.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// The globally unique ID of the machine. Useful when storing the results to disk, does not affect results in any way.
+        /// </summary>
         public string GUID { get; set; }
+
+        /// <summary>
+        /// The description of the machine. Useful when storing the results to disk, does not affect results in any way.
+        /// </summary>
         public string Description { get; set; }
-        public bool Trained { get; set; }
 
-        // input data
-        public IInputData<double> Inputs { get; set; }
-        public virtual double[] Outputs { get; set; }
-        public virtual double TestValue { get; set; }
-        //ITrainingData TrainingData { get; set; }
+        #endregion
 
-        // Learning & predicting
-        public object Algorithm { get; set; }
+        #region Algorithm
 
-        // Result
-        public double Result { get; set; }
+        /// <summary>
+        /// The algorithm that will be used for learning, hidden in Dynamo due to interface issues.
+        /// </summary>
+        [IsVisibleInDynamoLibrary(false)]
+        public IAlgorithm Algorithm { get; set; }
+
+        /// <summary>
+        /// Indicates whether training data was successfully loaded into the machine and that it has learned from it.
+        /// </summary>
+        public bool IsTrained => Algorithm.HasTrainingDataLoaded && Algorithm.IsTrained;
+
+        /// <summary>
+        /// The result supplied by the prediction algorithm used.
+        /// </summary>
+        public object Result => Algorithm.LastResult;
+
+        /// <summary>
+        /// The datatype of the result, useful for data validation.
+        /// </summary>
+        public string ResultDataType => Algorithm.ResultType.ToString();
+
+        /// <summary>
+        /// The last value used as input for Prediction.
+        /// </summary>
+        public object LastTestValue => Algorithm.LastTestValue;
+
+        // Type helpers
+        private Type AlgorithmClassType;
+
+        /// <summary>
+        /// Get the algorithm used by the machine for learning.
+        /// </summary>
+        /// <returns>The algorithm used by the machine for learning, as an object.</returns>
+        public dynamic GetAlgorithm()
+        {
+            return Convert.ChangeType(this.Algorithm, Algorithm.GetType());
+        }
+
+        /// <summary>
+        /// Sets the algorithm to the specified object, throws if argument is null or doesn't implement IAlgorithm.
+        /// </summary>
+        /// <param name="algorithm">The object to set algorithm to.</param>
+        private void SetAlgorithm(object algorithm)
+        {
+            // Check algorithm implements the IAlgorithm interface and it's not null.
+            // This is because the learning and prediction methods accept IAlgorithm objects only.
+            if (algorithm == null) throw new ArgumentNullException("The algorithm cannot be null.");
+            if (!(algorithm is IAlgorithm))
+                throw new Exception(
+                    "The supplied algorithm is not valid" + Environment.NewLine +
+                    "Expected : something implementing IAlgorithm" + Environment.NewLine +
+                    "Got : type " + algorithm.GetType().ToString() + " and interfaces " + algorithm.GetType().GetInterfaces().ToString()
+                    );
+            // ok to cast if we reached this point.
+            var algo = (IAlgorithm)algorithm;
+
+            // check we already have training data in the algorithm.
+            if (!algo.HasTrainingDataLoaded) throw new ArgumentException("The specified algorithm has no training data.");
+
+            // record the algorithm used as an object and its type, required in GetAlgorithm method.
+            this.Algorithm = algo;
+            this.AlgorithmClassType = algorithm.GetType();
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Build a new machine, to learn from training data and predict outcomes.
+        /// </summary>
+        /// <param name="algorithm">The algorithm to use for learning. Has to contain training data already.</param>
+        /// <param name="name">(optional) Specify a name for this machine.</param>
+        /// <param name="description">(optional) specify a description for this machine.</param>
+        public Machine(object algorithm, string name = null, string description = null)
+        {
+            // record the algorithm used as an object and its type, required in GetAlgorithm method.
+            SetAlgorithm(algorithm);
+
+            // default values
+            GUID = Guid.NewGuid().ToString();
+            Name = string.IsNullOrWhiteSpace(name) ? GUID : name;
+            Description = string.IsNullOrWhiteSpace(description) ? string.Empty : description;
+        }
+
+        /// <summary>
+        /// Public parameterless constructor needed for deserialisation.
+        /// </summary>
+        [IsVisibleInDynamoLibrary(false)]
+        public Machine() { }
 
         #endregion
 
         #region ML
 
-        /// The ML Learn and Predict functions should be abstract as each algorithm needs to have its own implementation.
-        /// This would however break serialisation in this base class, so they're virtual and explictly not implemented.
-
         /// <summary>
-        /// Enables a machine to learn from training data.
+        /// Tells a machine to learn from its training data.
         /// </summary>
-        /// <param name="machine">The machine that will learn.</param>
-        /// <param name="trainingData">The dataset used to train the machine.</param>
         /// <returns>The input machine, now trained.</returns>
-        public virtual IMachine Learn(object trainingData)
+        public IMachine Learn()
         {
-            throw new NotImplementedException();
+            Algorithm.Learn();
+            return this;
         }
 
         /// <summary>
         /// Enables a trained machine to provide a prediction from an input value.
         /// </summary>
-        /// <param name="machine">The machine that will predict, needs to be trained already.</param>
         /// <param name="inputData">Input for the prediction</param>
         /// <returns>The predicted value.</returns>
-        public virtual object Predict(object inputData)
+        public dynamic Predict(object inputData)
         {
-            throw new NotImplementedException();
+            // don't predict if we haven't learned the model yet
+            if (!this.IsTrained) throw new Exception("Cannot predict before the algorithm has learned.");
+
+            // check we haven't already predicted for this input and use cache if so
+            if (inputData == this.LastTestValue && this.IsTrained == true) return this.Result;
+
+            return Algorithm.Predict(inputData);
         }
 
         #endregion
@@ -63,12 +159,11 @@ namespace AI.Machine
         /// <summary>
         /// Serialises a machine to JSON and saves to specified file on disk.
         /// </summary>
-        /// <param name="machine">The machine to serialise.</param>
         /// <param name="filePath">The destination file on disk.</param>
         /// <returns>True if operation succeeded, false otherwise.</returns>
-        public virtual bool SaveModel(IMachine machine, string filePath)
+        public bool SaveModel(string filePath)
         {
-            return Json.ToJsonFile(machine, filePath);
+            return Json.ToJsonFile(this, filePath);
         }
 
         /// <summary>
@@ -76,7 +171,7 @@ namespace AI.Machine
         /// </summary>
         /// <param name="filePath">The JSON file to load from.</param>
         /// <returns>The trained machine. Throws Exception if deserialisation did not succeed.</returns>
-        public virtual IMachine LoadModel(string filePath)
+        public static Machine LoadModel(string filePath)
         {
             return Json.FromJsonFileTo<Machine>(filePath);
         }
