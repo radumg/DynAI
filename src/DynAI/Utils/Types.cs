314b67de5737038147c251fc076b4b4c73754e58
Radu Gidei <radugidei@gmail.com>
2019-07-17 15:27:06 +0100

    make type utils extensions & fix list verification

--- src/DynAI/Utils/Types.cs
+++ src/DynAI/Utils/Types.cs
@@ -6,25 +6,26 @@ namespace AI.Utils
 {
     internal static class Types
     {
-        public static bool IsList(object o)
+        public static bool IsList(this object o)
         {
             if (o == null) return false;
-            return o is IList &&
-                   o.GetType().IsGenericType &&
-                   o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+            var type = o.GetType();
+
+            // immediate confirmation of arrays and lists
+            if (type.IsArray) return true;
+            if (o is IList) return true;
+            if (o is IEnumerable) return true;
+
+            return type.IsGenericType &&
+                   type.IsAssignableFrom(typeof(List<>));
         }
 
-        public static bool IsDictionary(object o)
+        public static bool IsDictionary(this object o)
         {
             if (o == null) return false;
             return o is IDictionary &&
                    o.GetType().IsGenericType &&
                    o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(Dictionary<,>));
-        }
-
-        public static string[] IngestStringArray(string[] array)
-        {
-            return array;
         }
     }
 }
